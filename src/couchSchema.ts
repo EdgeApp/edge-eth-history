aa3db6e9d6e8f350240896d69d559ac5b302bb54
Lex Peterson <lpeterson@chorus.net>
2021-06-01 16:55:10 -0700

    Setup server and initialize database

---
+++ src/couchSchema.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-var */
+
+/**
+ * Describes a single database that should exist.
+ */
+export interface CouchDbInfo {
+  name: string
+  indexes?: Array<{
+    index: {
+      fields: string[]
+      partial_filter_selector?: any
+    }
+    ddoc: string
+    name: string
+    type: 'json'
+  }>
+  views?: Array<{
+    name: string
+    views: {
+      [viewName: string]: {
+        map?: string
+        reduce?: string
+      }
+    }
+  }>
+}
+
+export const couchSchema: CouchDbInfo[] = [
+  {
+    name: 'eth_history'
+  }
+]
