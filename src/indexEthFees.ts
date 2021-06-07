d808c5ad4b00367999554df5dbc411872444ff02
Lex Peterson <lpeterson@chorus.net>
2021-06-07 09:59:36 -0700

    Add ether chain fetch

--- src/indexEthFees.ts
+++ src/indexEthFees.ts
@@ -10,6 +10,15 @@ export interface GasInfo {
   source?: string
 }
 
+export interface GasInfo2 {
+  fast: number
+  fastest: number
+  safeLow: number
+  standard?: number
+  average?: number
+  source?: string
+}
+
 export const ethGasStationCall = async (): Promise<GasInfo> => {
   const options = {
     method: 'GET',
@@ -19,6 +28,18 @@ export const ethGasStationCall = async (): Promise<GasInfo> => {
     }
   }
   const url = `https://ethgasstation.info/api/ethgasAPI.json?api-key=${config.ethGasStationApiKey}`
+  const result = await fetch(url, options)
+  return result.json()
+}
+export const etherChainCall = async (): Promise<GasInfo2> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }
+  }
+  const url = `https://etherchain.org/api/gasPriceOracle`
   const result = await fetch(url, options)
   return result.json()
 }
