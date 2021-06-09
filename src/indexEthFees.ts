99fe9b755de3be6b4293a9c24e5c27d6946d7ca7
Lex Peterson <lpeterson@chorus.net>
2021-06-09 13:55:00 -0700

    fixup! Add eth gas station fetch

--- src/indexEthFees.ts
+++ src/indexEthFees.ts
@@ -1,6 +1,9 @@
+import { asNumber, asObject, asOptional } from 'cleaners'
 import fetch from 'node-fetch'
 
 import { config } from './config'
+
+const mylog = console.log
 
 export interface GasInfo {
   fast: number
@@ -19,7 +22,23 @@ export interface GasInfo2 {
   source?: string
 }
 
-export const ethGasStationCall = async (): Promise<GasInfo> => {
+const asEthGasStationResponse = asObject({
+  fast: asNumber,
+  fastest: asNumber,
+  safeLow: asNumber,
+  average: asNumber,
+  block_time: asNumber,
+  speed: asNumber,
+  safeLowWait: asNumber,
+  avgWait: asNumber,
+  fastWait: asNumber,
+  fastestWait: asNumber,
+  gasPriceRange: asObject({
+    asOptional
+  })
+})
+
+export const ethGasStationCall = async (): Promise<GasInfo | null> => {
   const options = {
     method: 'GET',
     headers: {
@@ -28,8 +47,19 @@ export const ethGasStationCall = async (): Promise<GasInfo> => {
     }
   }
   const url = `https://ethgasstation.info/api/ethgasAPI.json?api-key=${config.ethGasStationApiKey}`
-  const result = await fetch(url, options)
-  return result.json()
+  try {
+    const result = await fetch(url, options)
+    if (result.ok === false) {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      mylog(`Eth Gas Station returned code ${result.status}`)
+    }
+    const jsonObj = await result.json()
+    asEthGasStationResponse(jsonObj)
+    return jsonObj
+  } catch (e) {
+    mylog('Error is:', e)
+    return null
+  }
 }
 export const etherChainCall = async (): Promise<GasInfo2> => {
   const options = {
