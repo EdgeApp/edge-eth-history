2c70efd85ff4260abde7503ad646fa1282b3c793
Lex Peterson <lpeterson@chorus.net>
2021-06-21 16:06:02 -0700

    Add fetch call for EthGasStation fees

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,8 @@
+import fetch from 'node-fetch'
+
 import { config } from './config'
+
+const mylog = console.log
 
 export const snooze = async (ms: number): Promise<void> =>
   await new Promise((resolve: Function) => setTimeout(resolve, ms))
@@ -17,4 +21,30 @@ export function normalizeDate(dateSrc: string): string | null {
   dateNorm.setSeconds(0)
   dateNorm.setMilliseconds(0)
   return dateNorm.toISOString()
+}
+
+export const apiFetchCall = (
+  fetchCleaner: (value: any) => any,
+  url: string,
+  providerName: string
+) => async (): Promise<any | null> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }
+  }
+  try {
+    const result = await fetch(url, options)
+    if (result.ok === false) {
+      mylog(`${providerName} returned code ${JSON.stringify(result.status)}`)
+    }
+    const jsonObj = await result.json()
+    fetchCleaner(jsonObj)
+    return jsonObj
+  } catch (e) {
+    mylog('Error is:', e)
+    return null
+  }
 }
