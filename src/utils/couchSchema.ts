2259b24cd91b1b2f0c2b85c844e0d57608c73bfb
Lex Peterson <lpeterson@chorus.net>
2021-06-21 16:03:32 -0700

    Add couch schema to generate databases

---
+++ src/utils/couchSchema.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-var */
+
+/**
+ * Describes a single database that should exist.
+ */
+export interface CouchDbInfo {
+  name: string
+  indexes?: Array<{
+    index: {
+      fields: string[]
+      partial_filter_selector?: any
+    }
+    ddoc: string
+    name: string
+    type: 'json'
+  }>
+  views?: Array<{
+    name: string
+    views: {
+      [viewName: string]: {
+        map?: string
+        reduce?: string
+      }
+    }
+  }>
+}
+
+export const couchSchema: CouchDbInfo[] = [
+  {
+    name: 'fees_ethgasstation'
+  },
+  {
+    name: 'fees_mempoolspace'
+  },
+  {
+    name: 'fees_earn'
+  }
+]
