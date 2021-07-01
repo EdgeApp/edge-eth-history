8a24c3266501c6c4ac54a9e5395cc9f3d486ace7
Lex Peterson <>
2021-07-01 13:28:51 -0700

    Parse dates to improve readability

--- src/client/components/Graphs.tsx
+++ src/client/components/Graphs.tsx
@@ -4,6 +4,16 @@ interface GraphsState {
   data: any[]
 }
 
+const parseDate = (timestamp: string): string => {
+  const dateObj = new Date(timestamp)
+  const y = dateObj.getUTCFullYear()
+  const m = dateObj.getUTCMonth() + 1
+  const d = dateObj.getUTCDate()
+  const h = dateObj.getUTCHours()
+  const min = dateObj.getUTCMinutes()
+  return `${y}-${m}-${d}T${h}:${min}`
+}
+
 class Graphs extends Component<{}, GraphsState> {
   constructor(props) {
     super(props)
