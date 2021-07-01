a7de246ed1b17e400d171f1ee5b378aade2ab1e8
Lex Peterson <>
2021-07-01 13:30:45 -0700

    Separate provider data to enable graphing

--- src/client/components/Graphs.tsx
+++ src/client/components/Graphs.tsx
@@ -14,6 +14,39 @@ const parseDate = (timestamp: string): string => {
   return `${y}-${m}-${d}T${h}:${min}`
 }
 
+export const dataParser = (rawData: any, provider: string): any => {
+  let parsedData: any[] = []
+  const shortDateData: any[] = []
+  const datesNumber = rawData.length / 3
+  switch (provider) {
+    case 'Eth Gas Station':
+      parsedData = rawData.slice(0, datesNumber)
+      parsedData.forEach(object => {
+        const _id = parseDate(object._id)
+        const newObject = { ...object, _id }
+        shortDateData.push(newObject)
+      })
+      break
+    case 'Mempool.space':
+      parsedData = rawData.slice(datesNumber, datesNumber * 2)
+      parsedData.forEach(object => {
+        const _id = parseDate(object._id)
+        const newObject = { ...object, _id }
+        shortDateData.push(newObject)
+      })
+      break
+    case 'Earn':
+      parsedData = rawData.slice(datesNumber * 2, datesNumber * 3)
+      parsedData.forEach(object => {
+        const _id = parseDate(object._id)
+        const newObject = { ...object, _id }
+        shortDateData.push(newObject)
+      })
+      break
+  }
+  return shortDateData
+}
+
 class Graphs extends Component<{}, GraphsState> {
   constructor(props) {
     super(props)
