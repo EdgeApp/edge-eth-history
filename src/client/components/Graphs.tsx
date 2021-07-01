2a7c9cdaf8463621bb79a68c7ba69de6213b7208
Lex Peterson <>
2021-07-01 13:33:44 -0700

    Graph EthGasStation, Mempool, and Earn data

--- src/client/components/Graphs.tsx
+++ src/client/components/Graphs.tsx
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
-
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts'
 interface GraphsState {
   data: any[]
+}
+
+const graphHolder = {
+  height: '90%'
+}
+
+const graphTitle = {
+  marginLeft: '7rem'
 }
 
 const parseDate = (timestamp: string): string => {
@@ -56,7 +73,192 @@ class Graphs extends Component<{}, GraphsState> {
   }
 
   render(): JSX.Element {
-    return <></>
+    return (
+      <>
+        <div style={graphHolder}>
+          <h1 style={graphTitle}>Eth Gas Station</h1>
+          <ResponsiveContainer width="95%" height={400}>
+            <LineChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 0,
+                left: 20
+              }}
+              data={dataParser(this.state.data, 'Eth Gas Station')}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis dataKey="_id" />
+              <YAxis
+                yAxisId="left"
+                orientation="left"
+                stroke="#000000"
+                width={105}
+                label={{
+                  value: 'Gwei x 10',
+                  angle: -90,
+                  position: 'center'
+                }}
+              />
+              <Tooltip />
+              <Legend />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.fastest"
+                stroke="#0b84a5"
+                name="Fastest"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.fast"
+                stroke="#f6c860"
+                name="Fast"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.average"
+                stroke="#ca472f"
+                name="Average"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.safeLow"
+                stroke="#9dd865"
+                name="Safe Low"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+          <h1 style={graphTitle}>Mempool.space</h1>
+          <ResponsiveContainer width="95%" height={400}>
+            <LineChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 0,
+                left: 20
+              }}
+              data={dataParser(this.state.data, 'Mempool.space')}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis dataKey="_id" />
+              <YAxis
+                yAxisId="left"
+                orientation="left"
+                stroke="#000000"
+                width={105}
+                label={{
+                  value: 'satoshis/vbyte',
+                  angle: -90,
+                  position: 'center'
+                }}
+              />
+              <Tooltip />
+              <Legend />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.fastestFee"
+                stroke="#0b84a5"
+                name="Fastest Fee"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.halfHourFee"
+                stroke="#f6c860"
+                name="Half Hour Fee"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.hourFee"
+                stroke="#ca472f"
+                name="Hour Fee"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.minimumFee"
+                stroke="#9dd865"
+                name="Minimum Fee"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+          <h1 style={graphTitle}>Earn</h1>
+          <ResponsiveContainer width="95%" height={400}>
+            <LineChart
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 0,
+                left: 20
+              }}
+              data={dataParser(this.state.data, 'Earn')}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis dataKey="_id" />
+              <YAxis
+                yAxisId="left"
+                orientation="left"
+                stroke="#000000"
+                width={105}
+                label={{
+                  value: 'satoshis/byte',
+                  angle: -90,
+                  position: 'center'
+                }}
+              />
+              <Tooltip />
+              <Legend />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.sortedData.zeroToOne"
+                stroke="#0b84a5"
+                name="0-1"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.sortedData.oneToTwo"
+                stroke="#f6c860"
+                name="1-2"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.sortedData.twoToThree"
+                stroke="#ca472f"
+                name="2-3"
+              />
+              <Line
+                dot={false}
+                legendType="square"
+                yAxisId="left"
+                dataKey="data.sortedData.threeToTen"
+                stroke="#9dd865"
+                name="3-10"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </>
+    )
   }
 }
 
